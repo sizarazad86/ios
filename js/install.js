ca4d1eedc685d2babc0dc20d93a57d7d0161f155
Sizar Azad <sizarazad86@gmail.com>
2020-05-19 13:52:45 +0300

    update

--- js/install.js
+++ js/install.js
@@ -1,5 +1,22 @@
+/*
+
+App Information Installer By @s1z4r
+5/20/2020
+https://sizu.gq
+
+*/
+
 var $$ = Dom7;
 
+$$('.install-app').on('click', function (appName, bundle, version, size) {
+	app.dialog.confirm('App Name: ' + 'App Name Here' + '<br>Bundle Identifier: ' + 'Bundle Here' + '<br>Version: ' + 'Version Here' + '<br>Size: ' + 'Size Here', 'Information', function () {
+		app.dialog.preloader('Installing...', 'white');
+		window.location = "itms-services://?action=download-manifest&url=Link";
+		setTimeout(function () {
+			app.dialog.close();
+		}, 1500);
+	});
+});
 
 $$('.install-addme').on('click', function (appName, bundle, version, size) {
 	app.dialog.confirm('App Name: ' + 'AddMe' + '<br>Bundle Identifier: ' + 'com.PrestigeNetEnterprises.AddMe' + '<br>Version: ' + '1.4' + '<br>Size: ' + '26.8MB', 'Information', function () {
