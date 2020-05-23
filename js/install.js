1f81603e7553c8d7255051fe18f777deccc585dc
Sizar Azad <sizarazad86@gmail.com>
2020-05-23 05:14:53 +0300

    update

--- js/install.js
+++ js/install.js
@@ -19,7 +19,7 @@ $$('.install-app').on('click', function (appName, bundle, version, size) {
 });
 
 $$('.install-addme').on('click', function (appName, bundle, version, size) {
-	app.dialog.confirm('App Name: ' + 'AddMe' + '<br>Bundle Identifier: ' + 'com.PrestigeNetEnterprises.AddMe' + '<br>Version: ' + '1.4' + '<br>Size: ' + '26.8MB', 'Information', function () {
+	app.dialog.confirm('App Name: ' + 'AddMe' + '<br>Bundle Identifier: ' + 'com.PrestigeNetEnterprises.AddMe' + '<br>Version: ' + '1.4' + '<br>Size: ' + '26.8 MB', 'Information', function () {
 		app.dialog.preloader('Installing...', 'white');
 		window.location = "itms-services://?action=download-manifest&url=https://ios.sizu.gq/plists/addme.plist";
 		setTimeout(function () {
@@ -29,7 +29,7 @@ $$('.install-addme').on('click', function (appName, bundle, version, size) {
 });
 
 $$('.install-8ballpool').on('click', function (appName, bundle, version, size) {
-	app.dialog.confirm('App Name: ' + '8 Ball Pool' + '<br>Bundle Identifier: ' + 'com.miniclip.8ballpoolmult' + '<br>Version: ' + '4.8.4' + '<br>Size: ' + '53.6MB', 'Information', function () {
+	app.dialog.confirm('App Name: ' + '8 Ball Pool' + '<br>Bundle Identifier: ' + 'com.miniclip.8ballpoolmult' + '<br>Version: ' + '4.8.4' + '<br>Size: ' + '53.6 MB', 'Information', function () {
 		app.dialog.preloader('Installing...', 'white');
 		window.location = "itms-services://?action=download-manifest&url=https://ios.sizu.gq/plists/8ballpool.plist";
 		setTimeout(function () {
@@ -39,9 +39,19 @@ $$('.install-8ballpool').on('click', function (appName, bundle, version, size) {
 });
 
 $$('.install-pubgm').on('click', function (appName, bundle, version, size) {
-	app.dialog.confirm('App Name: ' + 'PUBG Mobile Hack' + '<br>Bundle Identifier: ' + 'com.tencent.ig' + '<br>Version: ' + '0.18.0' + '<br>Size: ' + '2GB', 'Information', function () {
+	app.dialog.confirm('App Name: ' + 'PUBG Mobile Hack' + '<br>Bundle Identifier: ' + 'com.tencent.ig' + '<br>Version: ' + '0.18.0' + '<br>Size: ' + '2 GB', 'Information', function () {
 		app.dialog.preloader('Installing...', 'white');
 		window.location = "itms-services://?action=download-manifest&url=https://ios.sizu.gq/plists/pubgm.plist";
+		setTimeout(function () {
+			app.dialog.close();
+		}, 1500);
+	});
+});
+
+$$('.install-minecraft').on('click', function (appName, bundle, version, size) {
+	app.dialog.confirm('App Name: ' + 'Minecraft' + '<br>Bundle Identifier: ' + 'com.mojang.minecraftpe' + '<br>Version: ' + '1.14.60' + '<br>Size: ' + '210.7 MB', 'Information', function () {
+		app.dialog.preloader('Installing...', 'white');
+		window.location = "itms-services://?action=download-manifest&url=https://ios.sizu.gq/plists/minecraftpe.plist";
 		setTimeout(function () {
 			app.dialog.close();
 		}, 1500);
